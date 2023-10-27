8d1de5fe0ca5f855ecf62d97ceca380c6730ffd0
tejas-su <tejas_s.u@yahoo.com>
2023-10-27 18:51:42 +0530

    Minor Changes

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -5,6 +5,60 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Center(
+          child: CircleAvatar(
+            radius: 100,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.person,
+                  size: 30,
+                ),
+                title: Text('tejas.s.u_'),
+                subtitle: Text(
+                  'This is not your username or pin.',
+                ),
+                trailing: Icon(Icons.edit),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.call,
+                  size: 30,
+                ),
+                title: Text('Phone'),
+                subtitle: Text(
+                  '+91 8277318798',
+                ),
+                trailing: Icon(Icons.edit),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.mail_rounded,
+                  size: 30,
+                ),
+                title: Text('E-mail'),
+                subtitle: Text(
+                  'tejas_s.u@yahoo.com',
+                ),
+                trailing: Icon(Icons.edit),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
